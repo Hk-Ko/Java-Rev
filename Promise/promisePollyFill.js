7dbc5538c875678dcf1d83e6a9616465bb1435e0
unknown <htunkko.mgk@gmail.com>
2024-05-30 12:36:35 +0400

    updated

---
+++ Promise/promisePollyFill.js
@@ -0,0 +1,77 @@
+function PromisePolyFill(executor) {
+  let onResolve,
+    onReject,
+    isFulFilled = false,
+    isRejected = false,
+    isCalled = false,
+    value;
+
+  function resolve(val) {
+    isFulFilled = true;
+    value = val;
+
+    if (typeof onResolve === "function") {
+      onResolve(val);
+      isCalled = true;
+    }
+  }
+
+  function reject(val) {
+    isRejected = true;
+    value = val;
+    if (typeof onReject === "function") {
+      onReject(val);
+      called = true;
+    }
+  }
+
+  this.then = function (callback) {
+    onResolve = callback;
+
+    if (isFulFilled && !isCalled) {
+      called = true;
+      onResolve(value);
+    }
+
+    return this;
+  };
+
+  this.catch = function (callback) {
+    onReject = callback;
+
+    if (isRejected && !isCalled) {
+      isCalled = true;
+      onReject(value);
+    }
+
+    return this;
+  };
+
+  try {
+    executor(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
+}
+
+const examplePromis = new PromisePolyFill((resolve, reject) => {
+  resolve(2);
+});
+
+examplePromis
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.error(err));
+
+PromisePolyFill.resolve = (val) => {
+  return new PromisePolyFill(function executor(resolve, reject) {
+    resolve(val);
+  });
+};
+
+PromisePolyFill.reject = (val) => {
+  return new PromisePolyFill(function executor(resolve, reject) {
+    reject(val);
+  });
+};
