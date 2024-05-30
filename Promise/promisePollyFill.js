0d04b909a1b81b6f3e2eb8442e3df620164ad84b
unknown <htunkko.mgk@gmail.com>
2024-05-30 12:43:59 +0400

    updated

--- Promise/promisePollyFill.js
+++ Promise/promisePollyFill.js
@@ -75,3 +75,29 @@ PromisePolyFill.reject = (val) => {
     reject(val);
   });
 };
+
+//Pollyfill for Promise.allPolyFill
+
+Promise.allPolyfill = (promises) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+
+    if (!promises.length) {
+      resolve(results);
+      return;
+    }
+
+    let pending = promises.length;
+
+    promises.forEach((promise, idx) => {
+      Promise.resolve(promise).then((res) => {
+        results[idx] = res;
+        pending--;
+
+        if (pending === 0) {
+          resolve(results);
+        }
+      }, reject);
+    });
+  });
+};
